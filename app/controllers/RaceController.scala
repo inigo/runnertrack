0589b0cb0483e500f6dfc7c2b948c8d4b1ab7d14
Inigo Surguy <inigo.surguy@gmail.com>
2015-05-16 16:36:47 +0100

    Display list of available races

--- app/controllers/RaceController.scala
+++ app/controllers/RaceController.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import net.surguy.runnertrack.enrich.{EnrichRunner, LinearFinishTimePredictor}
-import net.surguy.runnertrack.model.{Race, Distance, Distances}
+import net.surguy.runnertrack.model.{Distances, Race}
 import net.surguy.runnertrack.scraper.{CopenhagenMarathon2014Scraper, LondonMarathon2015Scraper, ManchesterMarathon2015Scraper, RaceScraper}
 import play.api.mvc.{Action, Controller}
 
@@ -18,19 +18,30 @@ object RaceController extends Controller {
     Ok(views.html.runners(race.name, richRunners.flatten))
   }
 
+  def listRaces() = Action { implicit request =>
+    Ok(views.html.listRaces(RaceLookup.raceNames))
+  }
+
+  def addRunners(raceId: String) = Action { implicit request =>
+    val raceName = RaceLookup.lookupId(raceId).name
+    Ok(views.html.addRunners(raceId, raceName))
+  }
+
 }
 
 /**
  * Convert a race name passed in to the browser to a name and associated code.
  */
 object RaceLookup {
-  def lookupId(raceId: String) = {
-    val browser = RaceScraper.browser()
-    raceId match {
-      case "manchester2015" => Race("Greater Manchester Marathon 2015", new ManchesterMarathon2015Scraper(browser), Distances.Marathon)
-      case "london2015" => Race("London Marathon 2015", new LondonMarathon2015Scraper(browser), Distances.Marathon)
-      case "copenhagen2014" => Race("Copenhagen 2014", new CopenhagenMarathon2014Scraper(browser), Distances.Marathon)
-    }
-  }
+  val browser = RaceScraper.browser()
+
+  private val races = Map(
+    "manchester2015" -> Race("Greater Manchester Marathon 2015", new ManchesterMarathon2015Scraper(browser), Distances.Marathon)
+    , "london2015" -> Race("London Marathon 2015", new LondonMarathon2015Scraper(browser), Distances.Marathon)
+    , "copenhagen2014" -> Race("Copenhagen 2014", new CopenhagenMarathon2014Scraper(browser), Distances.Marathon)
+  )
+
+  def lookupId(raceId: String): Race = races(raceId)
+  def raceNames: Seq[(String, String)] = races.map((tuple: (String, Race)) => (tuple._1, tuple._2.name) ).toSeq
 }
 
