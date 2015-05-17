f0f4b64090dd5218ef7d1e9865a48b24641c06b3
Inigo Surguy <inigo.surguy@gmail.com>
2015-05-17 12:03:34 +0100

    Add integration test for the pages

---
+++ test/controllers/IntegrationSpec.scala
@@ -0,0 +1,25 @@
+package controllers
+
+import org.junit.runner._
+import org.specs2.mutable._
+import org.specs2.runner._
+import play.api.test._
+
+@RunWith(classOf[JUnitRunner])
+class IntegrationSpec extends Specification {
+
+  "The home page" should {
+    "display a list of races" in new WithBrowser {
+      browser.goTo("http://localhost:" + port)
+      browser.pageSource must contain("Greater Manchester Marathon")
+      browser.pageSource must contain("London Marathon")
+    }
+  }
+
+  "The results page" should {
+    "show runners names" in new WithBrowser {
+      browser.goTo("http://localhost:" + port+"/manchester2015/513432")
+      browser.pageSource.toLowerCase must contain("Inigo Surguy".toLowerCase)
+    }
+  }
+}
